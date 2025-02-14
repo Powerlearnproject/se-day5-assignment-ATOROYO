bc9ae18d3dde917798bd2b365ed967136b933462
ATOROYO <davidatoroyosika@gmail.com>
2025-02-14 20:49:02 +0300

    Created the first component and activated stric mode

--- OneDrive/Desktop/REACT.JS/Startup files for react/03-pizza-menu/src/index.js
+++ OneDrive/Desktop/REACT.JS/Startup files for react/03-pizza-menu/src/index.js
@@ -1,2 +1,14 @@
 import React from "react";
 import reactDom from "react-dom/client";
+
+function App() {
+  return <h1>Hello React</h1>;
+}
+
+// React V18
+const root = reactDom.createRoot(document.getElementById(roor));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
